799cfd858fdb51d0a17dd79895b6f74f206adf9f
Alice Fortes <alicefortsp@gmail.com>
2022-04-22 00:47:14 -0300

    ajuste audio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import React, { useState } from 'react';
 import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
 function App() {
   const nome = "Alice Fortes"
   return (
     <div className="App">
-      <AudioPlayer 
-      src={process.env.PUBLIC_URL + '/audios/01.mp3'} />
+     
        <h1>
-         <p>Olá Mundo!</p>
+         <p>Olá, esta é uma música do Eve</p>
        </h1>
-       <p>Meu primeiro app do 0</p>
+       <div>
+       <AudioPlayer 
+      src={process.env.PUBLIC_URL + '/audios/01.mp3'} 
+      layout="stacked-reverse"/>
+       </div>
+       
        <p>Feito por {nome}</p>
 
     </div>
