d88a84190d46c6bd04e5109d085a8b5d16b52134
Alice Fortes <alicefortsp@gmail.com>
2022-04-22 00:41:49 -0300

    adicionando audio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import React, { useState } from 'react';
+import AudioPlayer from 'react-h5-audio-player'
 function App() {
   const nome = "Alice Fortes"
   return (
     <div className="App">
+      <AudioPlayer 
+      src={process.env.PUBLIC_URL + '/audios/01.mp3'} />
        <h1>
          <p>Olá Mundo!</p>
        </h1>
