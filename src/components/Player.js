92fc4b9760e96e4c101477cfe3a545d57a1189ae
Alice Fortes <alicefortsp@gmail.com>
2022-04-25 00:07:09 -0300

    .

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,6 +3,7 @@ import 'react-h5-audio-player/lib/styles.css';
 function Player(){
 return(
     <div>
+        <p>No componente</p>
     <AudioPlayer 
    src={process.env.PUBLIC_URL + '/audios/01.mp3'} 
    layout="stacked-reverse"/>
